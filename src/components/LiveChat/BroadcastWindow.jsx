eb8f41332418d32e503be235c2bd17b2d3ae557b
ish1n <ishangupta409@gmail.com>
2026-08-15 01:49:48 +0530

    fix: use independence_2026 template name

--- src/components/LiveChat/BroadcastWindow.jsx
+++ src/components/LiveChat/BroadcastWindow.jsx
@@ -714,7 +714,7 @@ export default function BroadcastWindow({
               >
                 <option value="fo_new_customers">New Customer Campaign (₹100 Credit)</option>
                 <option value="for_existingusers">Retention Campaign (₹80 Credit)</option>
-                <option value="independence_day">Independence Day Greeting (Aug 15)</option>
+                <option value="independence_2026">Independence Day Greeting (Aug 15)</option>
               </select>
             </div>
 
